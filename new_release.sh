b300038717056962e90c5ad0b0e3b3adf3bdd7d3
Franciszek Szeptycki <franciszek.szeptycki@goodylabs.com>
2025-08-26 09:54:14 +0200

    release script

--- new_release.sh
+++ new_release.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 
+bump_type=${1:-patch}
+
 latest_tag=$(git tag --sort=-v:refname | head -n1)
 
 if [ -z "$latest_tag" ]; then
-    new_tag="v0.0.1"
+    major=0
+    minor=0
+    patch=0
 else
     IFS='.' read -r major minor patch <<<"${latest_tag#v}"
-    patch=$((patch + 1))
-    new_tag="v${major}.${minor}.${patch}"
 fi
+
+case $bump_type in
+    major)
+        major=$((major + 1))
+        minor=0
+        patch=0
+        ;;
+    minor)
+        minor=$((minor + 1))
+        patch=0
+        ;;
+    patch)
+        patch=$((patch + 1))
+        ;;
+    *)
+        echo "Usage: $0 [major|minor|patch]"
+        exit 1
+        ;;
+esac
+
+new_tag="v${major}.${minor}.${patch}"
 
 while git rev-parse "$new_tag" >/dev/null 2>&1; do
     patch=$((patch + 1))
